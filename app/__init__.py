0a464ef4acf2326b54025d52e2649be43eb8b3f0
Christine <you@example.com>
2019-05-24 11:54:45 +0300

    modifies file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from .config import config_options
+from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -31,8 +31,6 @@ def create_app(config_name):
     simple.init_app(app)
 
     # setting config
-    from .request import configure_request
-    configure_request(app)
-
+    
 
     return app
