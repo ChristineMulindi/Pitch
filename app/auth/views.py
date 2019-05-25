c0f25436e2b2da2d80b15e781ea795d16cb6ddb1
Christine <you@example.com>
2019-05-25 07:21:58 +0300

    creates an authentication route logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,11 @@
-from flask import render_template, redirect, url_for
 from . import auth
+from flask import render_template, redirect, url_for
+from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import RegistrationForm
 from .. import db
+from ..email import mail_message
+
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -28,3 +31,9 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
