49e02cc323091232f9ac2db3e1b61ed4203a7d3b
Christine <you@example.com>
2019-05-25 07:36:50 +0300

    creates profile for user

--- app/models.py
+++ app/models.py
@@ -16,6 +16,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255), index = True)
     email = db.Column(db.String(255),unique = True, index = True)
     post_id = db.Column(db.Integer,db.ForeignKey('posts.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
 
     comments = db.relationship('Comment', backref='user', lazy="dynamic")
